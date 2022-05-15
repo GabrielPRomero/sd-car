d2755988a065649801a51e4f90409c13233da8fa
GabrielPRomero <80391539+GabrielPRomero@users.noreply.github.com>
2022-05-15 11:01:19 +0300

    impletemented ability to save best car to localstorage

--- index.js
+++ index.js
@@ -11,9 +11,21 @@ const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
 
 const N = 100;
 const cars = generateCars(N);
+let mainCar = cars[0];
+if (localStorage.getItem("bestBrain")) {
+  mainCar.brain = JSON.parse(localStorage.getItem("bestBrain"));
+}
 const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 
 animate();
+
+function save() {
+  localStorage.setItem("bestBrain", JSON.stringify(mainCar.brain));
+}
+
+function discard() {
+  localStorage.removeItem("bestBrain");
+}
 
 function generateCars(N) {
   const cars = [];
@@ -31,7 +43,7 @@ function animate(time) {
     cars[i].update(road.borders, traffic);
   }
 
-  const mainCar = cars.find(car => car.y == Math.min( ...cars.map(car => car.y)));
+  mainCar = cars.find((car) => car.y == Math.min(...cars.map((car) => car.y)));
   carCanvas.height = window.innerHeight;
   networkCanvas.height = window.innerHeight;
 
@@ -48,7 +60,6 @@ function animate(time) {
   }
   carCtx.globalAlpha = 1;
   mainCar.draw(carCtx, "blue", true);
-
 
   carCtx.restore();
 
