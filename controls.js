bfceb2cde79cf5f97daa53265a7678c760feba22
GabrielPRomero <80391539+GabrielPRomero@users.noreply.github.com>
2022-05-15 09:08:39 +0300

    Initial commit

---
+++ controls.js
@@ -0,0 +1,51 @@
+class Controls{
+    constructor(){
+        this.left = false;
+        this.right = false;
+        this.forward = false;
+        this.reverse = false;
+
+        // private method
+        this.#addKeyboardListeners();
+    }
+
+    #addKeyboardListeners(){
+        document.onkeydown = (e) => {
+            switch(e.key){
+                case 'ArrowLeft':
+                    this.left = true;
+                    break;
+                case 'ArrowRight':
+                    this.right = true;
+                    break;
+                case 'ArrowUp':
+                    this.forward = true;
+                    break;
+                case 'ArrowDown':
+                    this.reverse = true;
+                    break;
+                default:
+                    break;
+
+            }
+}
+        document.onkeyup = (e) => {
+            switch(e.key){
+                case 'ArrowLeft':
+                    this.left = false;
+                    break;
+                case 'ArrowRight':
+                    this.right = false;
+                    break;
+                case 'ArrowUp':
+                    this.forward = false;
+                    break;
+                case 'ArrowDown':
+                    this.reverse = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
